9a10a4e5beab770f463204f9e2fac466f89ed760
RajAman01 <Raj.aman4455@gmail.com>
2020-09-18 22:48:33 +0530

    updated ListView

--- app/src/main/java/com/example/securestore/MainActivity.kt
+++ app/src/main/java/com/example/securestore/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.securestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = arrayListOf<String>("Aman","Raj","Sarvesh","akansha","Rukaiya")
+        var image = arrayOf("1","2","3","4","5")
+        val myAdapter = ArrayAdapter<String>(this,R.layout.card_view,name)
+
+        val listView:ListView = findViewById<ListView>(R.id.mainListView)
+        listView.adapter = myAdapter
+
+
     }
 }
