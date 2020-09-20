1713ba7c056d0c1d63339b497c3dfd581036c929
LordSam <49715934+LordSam07@users.noreply.github.com>
2020-09-20 18:37:13 +0530

    Updated 4

--- app/src/main/java/com/lordsam/securestore/CreditDebitCardActivity.kt
+++ app/src/main/java/com/lordsam/securestore/CreditDebitCardActivity.kt
@@ -1,11 +1,27 @@
 package com.lordsam.securestore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class CreditDebitCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit_debit_card)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId){
+            R.id.menuItemAdd -> startActivity(Intent(this, PopUpCreditDebitActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/lordsam/securestore/PopUpCreditDebitActivity.kt
@@ -0,0 +1,11 @@
+package com.lordsam.securestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopUpCreditDebitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_credit_debit)
+    }
+}
