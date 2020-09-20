42e7ece9c135d95b2922c1deaecd9667f36d4f9b
RajAman01 <Raj.aman4455@gmail.com>
2020-09-20 16:23:54 +0530

    updated ListView 3

---
+++ app/src/main/java/com/example/securestore/CreditDebitCardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.securestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreditDebitCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit_debit_card)
+    }
+}
---
+++ app/src/main/java/com/example/securestore/EmailPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.securestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_password)
+    }
+}
