5fee6be7653399f9ed746e1d369b039cc93f6410
RajAman01 <Raj.aman4455@gmail.com>
2020-09-21 14:18:39 +0530

    Updated AdharDetailAdd.kt and card_adhar.xml

---
+++ app/src/main/java/com/lordsam/securestore/AdharDetailAdd.kt
@@ -0,0 +1,11 @@
+package com.lordsam.securestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdharDetailAdd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adhar_detail_add)
+    }
+}
