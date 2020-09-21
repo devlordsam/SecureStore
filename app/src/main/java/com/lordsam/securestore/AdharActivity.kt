463bc080f6205582186589ba3fbcd0c9e0ea208d
LordSam <49715934+LordSam07@users.noreply.github.com>
2020-09-21 15:49:03 +0530

    Updated 4

--- app/src/main/java/com/lordsam/securestore/AdharActivity.kt
+++ app/src/main/java/com/lordsam/securestore/AdharActivity.kt
@@ -1,11 +1,27 @@
 package com.lordsam.securestore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class AdharActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adhar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId){
+            R.id.menuItemAdd -> startActivity(Intent(this, PopUpAdharActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/lordsam/securestore/PopUpAdharActivity.kt
+++ app/src/main/java/com/lordsam/securestore/PopUpAdharActivity.kt
@@ -3,9 +3,9 @@ package com.lordsam.securestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AdharDetailAdd : AppCompatActivity() {
+class PopUpAdharActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adhar_detail_add)
+        setContentView(R.layout.activity_pop_up_adhar)
     }
 }
