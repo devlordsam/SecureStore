f0042b551165c68e1adeeee507567697d3212676
RajAman01 <Raj.aman4455@gmail.com>
2020-09-21 22:31:12 +0530

    Updated AdharDetailAdd.kt and card_adhar.xml

---
+++ app/src/main/java/com/lordsam/securestore/PopUpPANActivity.kt
@@ -0,0 +1,11 @@
+package com.lordsam.securestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopUpPANActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_p_a_n)
+    }
+}
